99bdac88626cb4c28fd3a26d8bb0a40df28a92f8
Anusha Polaki <108752874+Anusha019@users.noreply.github.com>
2023-07-13 12:46:21 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,7 @@
+import warnings
+
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
 
-st.title("Sales Analyzer")
+warnings.filterwarnings("ignore")
 
-df=pd.read_csv('Input_Sales_Data_v2.csv')
-df['Date'] = pd.to_datetime(df['Date'])
-min_date = df['Date'].min().to_pydatetime()
-max_date = df['Date'].max().to_pydatetime()
-
-min,max=st.slider('Select Date Range',min_value=min_date,max_value=max_date,value=(min_date,max_date))
-
-res = df.loc[(df['Date']>=min)&(df['Date']<=max)]
-df1=res.groupby('Manufacturer')[['Volume','Value']].sum().reset_index()
-
-st.write('### Total sold Volume and price from selected date')
-st.dataframe(df1)
-
-# Get the top 5 manufacturers for the selected period
-top_manufacturers=df1.sort_values('Value').tail(5)['Manufacturer'].unique()
-
-# Group the data by manufacturer and date to calculate the total sales over time
-sales = res.groupby(['Manufacturer', 'Date'], as_index=False)['Value'].sum()
-
-st.subheader('Top 5 manufacturers')
-plt.figure(figsize=(10, 6))
-for i in top_manufacturers:
-    manufacturer_data = sales[sales['Manufacturer'] == i]
-    plt.plot(manufacturer_data['Date'], manufacturer_data['Value'],label=i)
-
-plt.xlabel('Date')
-plt.ylabel('Sales')
-plt.title('Sales Trends for Top 5 Manufacturers')
-st.pyplot(plt)
-
-st.sidebar.image('logo.png',width=0.5,use_column_width=True)
-st.sidebar.markdown("---")
+st.markdown("<h1 style='text-align: center;'>Streamlit MultiPage Application</h1>", unsafe_allow_html=True)
